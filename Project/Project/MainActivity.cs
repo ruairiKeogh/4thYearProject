e7d2057e7db7a869dac7b55cd0206a6282185947
X00121581 <X00121581@computing.stu.it-tallaght.ie>
2017-10-17 15:52:42 +0100

    First Commit

---
+++ Project/Project/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Project
+{
+    [Activity(Label = "Project", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
